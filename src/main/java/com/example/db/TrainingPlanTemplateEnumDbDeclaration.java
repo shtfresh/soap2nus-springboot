84512f509bb3e80488333ad0341dc3c618259bbe
Rayes Huang <rayes.huang@oracle.com>
2019-05-15 13:48:56 +0800

    [DEVOPS-4782] add update tp status

---
+++ src/main/java/com/example/db/TrainingPlanTemplateEnumDbDeclaration.java
@@ -0,0 +1,104 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package com.example.db;
+
+import java.util.List;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.example.TrainingPlanTemplate.TrainingPlanTemplate;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class TrainingPlanTemplateEnumDbDeclaration {
+    
+    List<TrainingPlanTemplate> eList = null;
+	
+	private final Connection conn = DBConnectionMysql.getInstance().getConnection();
+	    /*
+	public JsonObject query(String sqlQueryStr) {
+		JsonObject tptEnumJsonObject = new JsonObject();
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQueryStr)) {
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {				
+		    	
+				tptEnumJsonObject.addProperty("tpStatus", rs.getString("tpStatus"));
+				tptEnumJsonObject.addProperty("status", rs.getString("status"));
+				tptEnumJsonObject.addProperty("task", rs.getString("task"));
+				tptEnumJsonObject.addProperty("tpTargetType", rs.getString("tpTargetType"));
+				tptEnumJsonObject.addProperty("tptType", rs.getString("tptType"));
+				tptEnumJsonObject.addProperty("run", rs.getString("pace_run"));
+				tptEnumJsonObject.addProperty("walk", rs.getString("pace_walk"));
+				tptEnumJsonObject.addProperty("easy", rs.getString("pace_easy"));
+				tptEnumJsonObject.addProperty("brisk", rs.getString("pace_brisk"));
+				tptEnumJsonObject.addProperty("jog", rs.getString("pace_jog"));
+				tptEnumJsonObject.addProperty("fast", rs.getString("pace_fast"));
+				tptEnumJsonObject.addProperty("recovery", rs.getString("pace_recovery"));
+
+			}
+		} catch (SQLException e) {
+            System.out.println("SQL Query Error: " + e.getMessage());
+		} catch (Exception e) {
+            System.out.println("Query Error: " + e.getStackTrace());
+		}
+		return tptEnumJsonObject;
+	}*/
+	
+	public JsonObject query(String sqlQueryStr) {
+		JsonObject tptEnumJsonObject = new JsonObject();
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQueryStr)) {
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {				
+		    	
+				tptEnumJsonObject.addProperty("tpStatus", rs.getString("tpStatus"));
+				tptEnumJsonObject.addProperty("status", rs.getString("status"));
+				tptEnumJsonObject.addProperty("task", rs.getString("task"));
+				tptEnumJsonObject.addProperty("tpTargetType", rs.getString("tpTargetType"));
+				tptEnumJsonObject.addProperty("tptType", rs.getString("tptType"));
+				tptEnumJsonObject.addProperty("run", rs.getString("pace_run"));
+				tptEnumJsonObject.addProperty("walk", rs.getString("pace_walk"));
+				tptEnumJsonObject.addProperty("easy", rs.getString("pace_easy"));
+				tptEnumJsonObject.addProperty("brisk", rs.getString("pace_brisk"));
+				tptEnumJsonObject.addProperty("jog", rs.getString("pace_jog"));
+				tptEnumJsonObject.addProperty("fast", rs.getString("pace_fast"));
+				tptEnumJsonObject.addProperty("recovery", rs.getString("pace_recovery"));
+
+			}
+		} catch (SQLException e) {
+            System.out.println("SQL Query Error: " + e.getMessage());
+		} catch (Exception e) {
+            System.out.println("Query Error: " + e.getStackTrace());
+		}
+		return tptEnumJsonObject;
+	}
+	
+    public boolean update(String tpOwnerId, String weeks){
+		String updateTableSQL = "UPDATE t_oracle_tp SET weeks=? WHERE tpOwnerId=?";
+		try (PreparedStatement preparedStatement = this.conn
+				.prepareStatement(updateTableSQL);) {
+			preparedStatement.setString(1, weeks);
+			preparedStatement.setString(2, tpOwnerId);
+
+			preparedStatement.executeUpdate();
+            return true;
+		} catch (SQLException e) {
+            System.out.println("SQL Update Error: "	+ e.getMessage());
+            return false;            
+		} catch (Exception e) {
+            System.out.println("Update Error: "	+ e.getMessage());
+            return false;            
+		}
+    
+    }
+    
+    public JsonObject getPlanTemplateEnum(){
+		String queryStr = "SELECT * FROM t_oracle_tptenum";
+		return this.query(queryStr);
+                   
+    }
+}
--- src/main/java/com/example/helloworld/TrainingPlanController.java
+++ src/main/java/com/example/helloworld/TrainingPlanController.java
@@ -4,7 +4,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import com.example.db.TrainingPlanTemplateDbDeclaration;
 import com.example.TrainingPlan.TrainingPlan;
 import com.example.TrainingPlanTemplate.TrainingPlanTemplate;
 import com.example.db.TrainingPlanDbDeclaration;
+import com.example.db.TrainingPlanTemplateEnumDbDeclaration;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -31,6 +34,7 @@ public class TrainingPlanController {
     TrainingPlanTemplateDbDeclaration edaoTpt = new TrainingPlanTemplateDbDeclaration();
     
     TrainingPlanDbDeclaration edaoTp = new TrainingPlanDbDeclaration();
+    TrainingPlanTemplateEnumDbDeclaration edaoTpEnum = new TrainingPlanTemplateEnumDbDeclaration();
 
     @RequestMapping(value="/activetp/{tpOwnerId}",method=RequestMethod.GET)
     public TrainingPlan getPlanActive(@PathVariable String tpOwnerId) {
@@ -57,6 +61,8 @@ public class TrainingPlanController {
     public String createPlan(@RequestBody String tpItem) {
     	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     	
+    	JsonObject tptEnumJsonObject = edaoTpEnum.getPlanTemplateEnum();
+    	
     	JsonObject tpItemJsonObject = new JsonParser().parse(tpItem).getAsJsonObject();
     	tpItemJsonObject.addProperty("tpId", "tpXXX"+counter.incrementAndGet());
     	tpItemJsonObject.addProperty("tpPublishedAt", df.format(new Date()));
@@ -72,6 +78,8 @@ public class TrainingPlanController {
     	if (tpt == null) {
     		return "Fail: "+tpItemJsonObject.get("tptId").toString()+" Not Found in TrainingPlan Template!";
     	}
+    	//System.out.println(tptEnumJsonObject);
+    	//return tptEnumJsonObject.get("tpStatus").toString();
     	
     	//System.out.println(tpt.gettptId());
     	//System.out.println(tpt.getWeeks());
@@ -83,21 +91,71 @@ public class TrainingPlanController {
         	Set<Map.Entry<String, JsonElement>> entries = temp.entrySet();
         	for (Map.Entry<String, JsonElement> entry: entries) {
         		//System.out.println(entry.getKey());
-        		if (entry.getValue().getAsJsonObject().get("tasks") != null) {
-        			startTime = startTime.plusDays(1);
-        			entry.getValue().getAsJsonObject().addProperty("date", startTime.toString());
-        			entry.getValue().getAsJsonObject().addProperty("status", "planed");
-        			//System.out.println(startTime.plusDays(1).toString());
-        			//tpItemJsonObject.get("tpStart").toString()
-        			
-        			//System.out.println(entry.getValue().getAsJsonObject().toString());
-        		}
+
+        		JsonElement taskItem = entry.getValue().getAsJsonObject().get("tasks");
+        		if (taskItem != null) {
+    				int minKilometre = 0;
+    				int maxKilometre = 0;
+        			for (JsonElement pb : taskItem.getAsJsonArray()) {
+        				int repeat = 0;
+        				repeat = Integer.parseInt(pb.getAsJsonObject().get("Re").toString());
+        				
+        				//System.out.println(repeat);
+        				for (JsonElement pasubTask : pb.getAsJsonObject().get("task").getAsJsonArray()) {
+            				int min = 0;
+            				int max = 0;
+            				int value = 0;
+        					JsonObject temp1 = pasubTask.getAsJsonObject();
+        			       	Set<Map.Entry<String, JsonElement>> subTaskentries = temp1.entrySet();
+        		        	for (Map.Entry<String, JsonElement> subTaskentry: subTaskentries) {
+        				//for (Map.Entry<String, JsonElement> entrysubTask: pb.getAsJsonObject().get("task").getAsJsonArray().get(0).getAsJsonObject().entrySet()) {
+        				    //System.out.println("Key: "+entrysubTask.getKey()+" Value: "+entrysubTask.getValue());
+        		        		String task_key = subTaskentry.getKey().toString();
+        		  
+        		        		value = Integer.parseInt(subTaskentry.getValue().toString());
+        		        		if (task_key.equals(new String("kilometre"))) {
+        		        			break;
+        		        		}
         		
+        		        		String tempString = tptEnumJsonObject.get(task_key).toString().replace("\\", "");
+        		        		tempString = tempString.substring(1, tempString.length()-1);
+        		        		//System.out.println(tempString);
+        		        		min = Integer.parseInt(new JsonParser().parse(tempString).getAsJsonObject().get("min").toString().replace("\"", ""));
+        		        		max = Integer.parseInt(new JsonParser().parse(tempString).getAsJsonObject().get("max").toString().replace("\"", ""));
+        		        		
+        		        		//System.out.println(new JsonParser().parse(tptEnumJsonObject.get(entrysubTask.getKey()).toString()).getAsJsonObject().get("max"));
+        		        	}
+        		        	if (min != 0 && max != 0) {
+        		        		minKilometre = minKilometre + ((1000/min) * value)*repeat;
+            		        	maxKilometre = maxKilometre + ((1000/max)* value)*repeat;
+        		        	} else {
+        		        		minKilometre = minKilometre + value*repeat*1000;
+            		        	maxKilometre = maxKilometre + value*repeat*1000;
+        		        	}
+        				}
+    		        	
+    		        	//System.out.println(pb.getAsJsonObject().get("task"));
+
+        				//System.out.println("minKilometre = "+division(minKilometre, 1000));
+            			//System.out.println("maxKilometre = "+division(maxKilometre, 1000));
+            			entry.getValue().getAsJsonObject().addProperty("minKilometre", division(minKilometre, 1000));
+            			entry.getValue().getAsJsonObject().addProperty("maxKilometre", division(maxKilometre, 1000));
+        				entry.getValue().getAsJsonObject().addProperty("finished", "0");
+        					
+        				startTime = startTime.plusDays(1);
+        				entry.getValue().getAsJsonObject().addProperty("date", startTime.toString());
+        				entry.getValue().getAsJsonObject().addProperty("status", "planed");
+        				//System.out.println(startTime.plusDays(1).toString());
+        				//tpItemJsonObject.get("tpStart").toString()
+        				//System.out.println(entry.getValue().getAsJsonObject().toString());
+        			}
+        		
+        		}
         	}
     	}
+    	
     	//System.out.println("******************");
     	System.out.println(weeksjsonArray.toString());
-    	
     	boolean result;
         
     	result = edaoTp.add(new TrainingPlan(
@@ -125,21 +183,66 @@ public class TrainingPlanController {
         }
     }
     
-    /*
-    @RequestMapping(value="/tptemplates/{tptId}",method=RequestMethod.PUT)
-    public String updatePlanTemplate(@PathVariable String tptId, @RequestBody String tptItem) {
+    @RequestMapping(value="/tpstatus",method=RequestMethod.POST)
+    public String updatePlanStatus(@RequestBody String tpItem) {
     	
-    	JsonObject tptItemJsonObject = new JsonParser().parse(tptItem).getAsJsonObject();
+    	JsonObject tpItemJsonObject = new JsonParser().parse(tpItem).getAsJsonObject();
+    	boolean result;
+    	String tpOwnerId = tpItemJsonObject.get("tpOwnerId").toString().replace("\"", "");
+    	TrainingPlan tp = edaoTp.getAllPlan(tpOwnerId).get(0);
+    	/*
+    	LocalDate tpStartTime = LocalDate.parse(tp.gettpStart().replace("\"", ""));
+    	LocalDate tpUpdateDate = LocalDate.parse(tpItemJsonObject.get("tpDate").toString().replace("\"", ""));
     	
-    	edao.update(tptId, new TrainingPlanTemplate(tptItemJsonObject.get("tptId").toString(),
-    			tptItemJsonObject.get("tptTile").toString(),
-    			tptItemJsonObject.get("tptType").toString(),
-    			tptItemJsonObject.get("tptDescrition").toString(),
-    			tptItemJsonObject.get("publishedAt").toString(),
-    			tptItemJsonObject.get("weeks").toString()));
-
-        return "Success";
-    }*/
+    	int daysDelta = (int)Duration.between(tpStartTime.atStartOfDay(), tpUpdateDate.atStartOfDay()).toDays();
+    	int week = daysDelta/7;
+    	String days = "day"+daysDelta%7;
+    	System.out.println("week = "+week+" days = "+days);
+    	*/
+    	
+    	JsonArray weeksjsonArray = new JsonParser().parse(tp.getWeeks()).getAsJsonArray();
+    	//System.out.println(weeksjsonArray.toString());
+    	//System.out.println(weeksjsonArray.get(week).getAsJsonObject().get(days).toString());
+    	
+    	for (JsonElement pa : weeksjsonArray) {
+    		JsonObject temp = pa.getAsJsonObject();
+    		
+        	Set<Map.Entry<String, JsonElement>> entries = temp.entrySet();
+        	for (Map.Entry<String, JsonElement> entry: entries) {
+        		JsonElement taskItem = entry.getValue().getAsJsonObject().get("tasks");
+        		if (taskItem != null) {
+        			//System.out.println(entry.getValue().getAsJsonObject().get("date").toString().replace("\"", ""));
+        			//System.out.println(tpItemJsonObject.get("tpDate").toString());
+        			if (entry.getValue().getAsJsonObject().get("date").toString().replace("\"", "").equals(new String(tpItemJsonObject.get("tpDate").toString().replace("\"", "")))) {
+		        		float kilometers = Float.parseFloat((tpItemJsonObject.get("kilometers").toString().replace("\"", "")));
+	    				float minKilometre = Float.parseFloat(entry.getValue().getAsJsonObject().get("minKilometre").toString().replace("\"", ""));
+	    				float maxKilometre = Float.parseFloat(entry.getValue().getAsJsonObject().get("maxKilometre").toString().replace("\"", ""));
+	    				//System.out.println("kilometers = "+kilometers+" minKilometre = "+minKilometre);
+	    				if (kilometers > Float.parseFloat(entry.getValue().getAsJsonObject().get("finished").toString().replace("\"", ""))) {
+	    					entry.getValue().getAsJsonObject().addProperty("finished", Float.toString(kilometers));
+	    				}
+		        		if (kilometers > minKilometre) {
+		        			//System.out.println("change status");
+		        			//System.out.println(entry.getValue().getAsJsonObject().get("status").toString());
+		        			entry.getValue().getAsJsonObject().addProperty("status", "completed");
+		        		} else {
+		        			entry.getValue().getAsJsonObject().addProperty("status", "incompleted");
+		        		}
+		        		
+		        			
+		        	}
+        		}
+        	}
+        }
+    	
+    	System.out.println(weeksjsonArray.toString());
+    	result = edaoTpEnum.update(tpOwnerId, weeksjsonArray.toString());
+        if (result) {
+        	return "success";
+        } else {
+        	return "fail";
+        }
+    }
     
     @RequestMapping(value="/tp/{tpId}", method=RequestMethod.DELETE)
     public String deletePlanTemplate(@PathVariable String tpId) {
@@ -151,5 +254,17 @@ public class TrainingPlanController {
         	return "fail";
         }
        
+    }
+    
+	public static String division(int a ,int b){
+        String result = "";
+        float num =(float)a/b;
+
+        DecimalFormat df = new DecimalFormat("0.0");
+
+        result = df.format(num);
+
+        return result;
+
     }
 }
