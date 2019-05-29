ebec55b22a4dfaae44ae393024498bae61c07388
Rayes Huang <rayes.huang@oracle.com>
2019-05-29 19:15:42 +0800

    [DEVOPS-4782] Fix database.properties path issue attempt=2

--- src/main/java/com/example/db/DBConfigProperties.java
+++ src/main/java/com/example/db/DBConfigProperties.java
@@ -20,14 +20,34 @@ public class DBConfigProperties {
 		pp = new Properties();
 		InputStream fps = null;
 		try	{
-			//Path basepath = Paths.get(".").toAbsolutePath().normalize();
-			//System.out.println(basepath);
+			Path basepath = Paths.get(".").toAbsolutePath().normalize();
+			System.out.println(basepath);
+			File folder = new File(basepath.toString());
+			for (String file : folder.list()) {
+				System.out.println(file);
+			}
+			
+			System.out.println("### main ###");
+			for (String file : new File("main").list()) {
+				System.out.println(file);
+			}
+			
+			System.out.println("### src ###");
+			for (String file : new File("src").list()) {
+				System.out.println(file);
+			}
+			
+
+			
+			
+			/*
 			File path = new File(ResourceUtils.getURL("classpath:").getPath());
 			String strDirPath = path.getAbsolutePath();
 			System.out.println(strDirPath);
 			strDirPath=strDirPath.replace("\\target\\classes", "");
 			System.out.println(strDirPath);
-			fps = new BufferedInputStream(new FileInputStream(new File(strDirPath+"/resources/database.properties"))); 
+			fps = new BufferedInputStream(new FileInputStream(new File(strDirPath+"/resources/database.properties")));*/
+			fps = new BufferedInputStream(new FileInputStream(new File(basepath+"/src/main/resources/database.properties")));
 			pp.load(fps);
 		} catch (IOException e) {
 			System.out.println("Read database.properties file failed !");
