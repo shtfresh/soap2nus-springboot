6a77eb10847f45d713e6aea92ac5f14046bef8ad
Rayes Huang <rayes.huang@oracle.com>
2019-06-18 16:41:01 +0800

    [DEVOPS-4782] update sql

--- dbScript/update.sql
+++ dbScript/update.sql
@@ -1,1 +1,2 @@
-alter table t_oracle_tp add column totalDays int(50) after maxKilometre
+alter table t_oracle_tp add column totalDays int(50) after maxKilometre;
+update t_oracle_tp set totalDays=0;
